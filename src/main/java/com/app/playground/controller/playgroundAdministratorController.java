573b0fe4706c37635395e4e95ed0b3e75470b305
projectJun <projectJun6460@gmail.com>
2023-10-12 17:02:49 +0900

    template 부분과 administrator Controller 정리 완료

--- src/main/java/com/app/playground/controller/playgroundAdministratorController.java
+++ src/main/java/com/app/playground/controller/playgroundAdministratorController.java
@@ -12,9 +12,69 @@ import org.springframework.web.servlet.view.RedirectView;
 @Controller
 @Slf4j
 @RequiredArgsConstructor
-@RequestMapping("/KHJ/templates/administrator_page/*")
+@RequestMapping("/administrator/*")
 public class playgroundAdministratorController {
 
-    @GetMapping("administrator_page_main_no_list_postManage")
-    public void Move_No_List_PostManage(){;}
+    @GetMapping("administratorIdFind")
+    public void Move_administratorIdFind(){;}
+
+    @GetMapping("administratorIdFindSuccess")
+    public void Move_administratorIdFindSuccess(){;}
+
+    @GetMapping("administratorJoin")
+    public void Move_administratorJoin(){;}
+
+    @GetMapping("administratorLogin")
+    public void Move_administratorLogin(){;}
+
+    @GetMapping("administratorPageMainNoListPostManage")
+    public void Move_administratorPageMainNoListPostManage(){;}
+
+    @GetMapping("administratorPageMainNoListQuestionManage")
+    public void Move_administratorPageMainNoListQuestionManage(){;}
+
+    @GetMapping("administratorPageMainNoListSchoolManage")
+    public void Move_administratorPageMainNoListSchoolManage(){;}
+
+    @GetMapping("administratorPageMainNoListUserManage")
+    public void Move_administratorPageMainNoListUserManage(){;}
+
+    @GetMapping("administratorPageMainUseListPostManage")
+    public void Move_administratorPageMainUseListPostManage(){;}
+
+    @GetMapping("administratorPageMainUseListQuestionManage")
+    public void Move_administratorPageMainUseListQuestionManage(){;}
+
+    @GetMapping("administratorPageMainUseListSchoolManage")
+    public void Move_administratorPageMainUseListSchoolManage(){;}
+
+    @GetMapping("administratorPageMainUseListUserManage")
+    public void Move_administratorPageMainUseListUserManage(){;}
+
+    @GetMapping("administratorPagePostDetail")
+    public void Move_administratorPagePostDetail(){;}
+
+    @GetMapping("administratorPagePostDetail2")
+    public void Move_administratorPagePostDetail2(){;}
+
+    @GetMapping("administratorPagePostDetailNoComment")
+    public void Move_administratorPagePostDetailNoComment(){;}
+
+    @GetMapping("administratorPagePostFixAndDel")
+    public void Move_administratorPagePostFixAndDel(){;}
+
+    @GetMapping("administratorPagePostFixAndDel2")
+    public void Move_administratorPagePostFixAndDel2(){;}
+
+    @GetMapping("administratorPageRegisterSchool")
+    public void Move_administratorPageRegisterSchool(){;}
+
+    @GetMapping("administratorPageRegisterUsers")
+    public void Move_administratorPageRegisterUsers(){;}
+
+    @GetMapping("administratorPasswordFind")
+    public void Move_administratorPasswordFind(){;}
+
+    @GetMapping("administratorPasswordFindSuccess")
+    public void Move_administratorPasswordFindSuccess(){;}
 }
